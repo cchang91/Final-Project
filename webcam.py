594b444a5b728580e4df944191a49a3c1763ad57
jkelly34 <162471375+jkelly34@users.noreply.github.com>
2024-05-09 19:28:59 -0400

    Update webcam.py
    added comments 1hr

--- webcam.py
+++ webcam.py
@@ -11,6 +11,8 @@ model = get_roboflow_model(
     api_key="GE5hI6Y9Set5IYtYPU13"
 )
 
+#The above code was inspired by roboflow's inference deployment code found on the blog screenshot posted on github:
+
 # Open the default camera (usually the built-in webcam)
 cap = cv2.VideoCapture(0)
 
@@ -19,6 +21,10 @@ if not cap.isOpened():
     print("Error: Could not open camera.")
     exit()
 
+"""This while loop was our attempt to deploy our model on a livestream, streaming from our webcam. At the end of the day,
+we found it to be too laggy and crashed too often to be implemented and utilized.
+
+"""
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -40,7 +46,7 @@ while True:
                 label = pred['class']
                 confidence = pred['confidence']
 
-                # Calculate corner points
+                # Calculate corner points of the box
                 x1, y1 = int(x - width / 2), int(y - height / 2)
                 x2, y2 = int(x + width / 2), int(y + height / 2)
 
@@ -51,7 +57,7 @@ while True:
         # Display the resulting frame
         cv2.imshow('Webcam Feed', frame)
 
-        # Press 'q' to quit the video window
+        # Press 'q' to quit the video window to get out of lag or stop code
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
